d864ac8853fe45683e16f66536cfdfdec5442bfb
cbrooks-dev <connorbrooks04@yahoo.com>
2025-06-14 20:41:57 -0400

    Add ability for user to delete crypto

--- application/homepage.py
+++ application/homepage.py
@@ -11,17 +11,17 @@ def home():
 
 @bp.route("/add_crypto", methods=["POST"])
 def add_crypto():
+    database = db.get_db()
+
+    if not database:
+        return jsonify({"error": "Database connection failed"})
+
     try:
         user_id = request.json.get("user_id")
         crypto_name = request.json.get("crypto_name")
         crypto_symbol = request.json.get("crypto_symbol")
     except TypeError:
         return jsonify({"error": "Invalid input"})
-
-    database = db.get_database()
-
-    if not database:
-        return jsonify({"error": "Database connection failed"})
     
     database.execute(
         "INSERT INTO crypto (user_id, crypto_name, crypto_symbol) VALUES (?, ?, ?)",
@@ -29,5 +29,31 @@ def add_crypto():
     )
 
     database.commit()
+    database.close()
 
     return jsonify({"message": "Crypto added successfully"})
+
+
+@bp.route("/delete_crypto", methods=["POST"])
+def delete_crypto():
+    database = db.get_db()
+
+    if not database:
+        return jsonify({"error": "Database connection failed"})
+
+    try:
+        user_id = request.json.get("user_id")
+        crypto_name = request.json.get("crypto_name")
+        crypto_symbol = request.json.get("crypto_symbol")
+    except TypeError as e:
+        return jsonify({"error": "Invalid input"})
+        
+    database.execute(
+        "DELETE FROM crypto WHERE user_id = ? AND crypto_name = ? AND crypto_symbol = ?",
+        (user_id, crypto_name, crypto_symbol)
+    )
+
+    database.commit()
+    database.close()
+
+    return jsonify({"message": "Crypto deleted successfully"})
