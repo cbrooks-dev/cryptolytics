1ae48343534c01254c60ecba68823dce150f3c1a
cbrooks-dev <connorbrooks04@yahoo.com>
2025-06-21 23:41:16 -0400

    Add more button functions and add to homepage

--- application/static/script.js
+++ application/static/script.js
@@ -20,3 +20,25 @@ function register() {
             console.log("Error: ", error)
         });
 }
+
+function getStarted() {
+    fetch("/get-started")
+        .then(response => response.json())
+        .then(data => {
+            alert(data.message)
+        })
+        .catch(error => {
+            console.log("Error: ", error)
+        });
+}
+
+function search() {
+    fetch("/search")
+        .then(response => response.json())
+        .then(data => {
+            alert(data.message)
+        })
+        .catch(error => {
+            console.log("Error: ", error)
+        });
+}
