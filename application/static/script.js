ee6fb521b36783f4e3d9d2af1e8e1521d2a3aa50
cbrooks-dev <connorbrooks04@yahoo.com>
2025-06-21 22:08:37 -0400

    Add scripts for login & register

---
+++ application/static/script.js
@@ -0,0 +1,22 @@
+function login() {
+    fetch("login")
+        .then(response => response.json())
+        .then(data => {
+            alert(data.message)
+        })
+        .catch(error => {
+            console.error("Error: ", error)
+        });
+}
+
+
+function register() {
+    fetch("register")
+        .then(response => response.json())
+        .then(data => {
+            alert(data.message)
+        })
+        .catch(error => {
+            console.log("Error: ", error)
+        });
+}
