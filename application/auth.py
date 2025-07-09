9fd29c9dd9cdf72df47d2fa2601bdccbec84de98
cbrooks-dev <connorbrooks04@yahoo.com>
2025-07-08 21:08:22 -0400

    Fix endpoint

--- application/auth.py
+++ application/auth.py
@@ -60,7 +60,7 @@ def login():
         if error is None:
             session.clear()
             session['user_id'] = user['id']
-            return redirect(url_for('index')) # TODO: fix endpoint
+            return redirect(url_for('home'))
 
         flash(error)
 
